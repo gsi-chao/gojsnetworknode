0344731a7c95b5161aeeb90eface8952cdfb9250
gsi-chao <chao@generalsoftwareinc.com>
2019-01-13 15:31:04 -0500

    First Commit Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, ElementRef} from '@angular/core';
+import * as go from 'gojs';
+import icon from '../assets/images/nnode.png';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'gojs';
+  node_data: any[];
+  link_data: any[];
+  @ViewChild('text')
+  private textField: ElementRef;
+
+  data: any;
+  node: go.Node;
+
+  constructor() {
+    this.node_data = [
+      {key: 1, text: 'Alpha 123 nuevo beta', color: 'lightblue', ip: '192.168.1.1', status: 'lightgray', icon: icon},
+      {key: 2, text: 'Beta  123 nuevo beta', color: 'orange', ip: '192.168.1.4', status: 'green', icon: icon},
+      {key: 3, text: 'Gamma  123 nuevo beta', color: 'lightgreen', ip: '192.168.1.5', status: 'green', icon: icon},
+      {key: 4, text: 'Delta  123 nuevo beta', color: 'pink', ip: '192.168.1.6', status: 'green', icon: icon}
+    ];
+    this.link_data = [
+      {from: 1, to: 2},
+      {from: 1, to: 3},
+      {from: 3, to: 4},
+      {from: 4, to: 1}
+    ];
+  }
+
+
+  showDetails(node: go.Node | null) {
+    this.node = node;
+    if (node) {
+      this.data = {
+        text: node.data.text,
+        color: node.data.color
+      };
+    } else {
+      this.data = null;
+    }
+  }
+
+  onModelChanged(c: go.ChangedEvent) {
+    this.showDetails(this.node);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NetworkNodeComponent} from './network-node/network-node.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NetworkNodeComponent
   ],
   imports: [
     BrowserModule,
